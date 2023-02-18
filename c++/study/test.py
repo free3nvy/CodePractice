1440471f03299d1d64a5b6117bdb0e7fc41418b1
Jacky Qu <free3nvy@gmail.com>
2023-02-18 08:59:16 +0800

    0218

--- c++/study/test.py
+++ c++/study/test.py
@@ -1,2 +1,98 @@
-imitate = ['http://10.244.161.33/01.m3u8', 'http://10.244.38.184/01.m3u8', 'http://10.244.90.163/01.m3u8']
-print(imitate[26%3])
+import time
+
+
+imitate_1k = ['http://10.244.161.98/01.m3u8', 'http://10.244.90.187/01.m3u8', 'http://10.244.38.123/01.m3u8']
+
+imitate_2k = ['http://10.244.163.230/01.m3u8', 'http://10.244.163.238/01.m3u8', 'http://10.244.38.187/01.m3u8']
+imitate_4k = ['http://10.244.161.72/01.m3u8', 'http://10.244.163.65/01.m3u8', 'http://10.244.163.231/01.m3u8']
+imitate_8k = ['http://10.244.90.120/01.m3u8', 'http://10.244.38.111/01.m3u8', 'http://10.244.161.89/01.m3u8']
+imitate_720p = ['http://10.244.161.87/01.m3u8', 'http://10.244.163.104/01.m3u8', 'http://10.244.38.36/01.m3u8']
+
+
+
+    
+
+def circle_add(i, imitate, ystenlive):
+    # 2分钟一次循环
+    
+    for i in range(173,174):
+        test = 'test'+ str(i)
+
+        m4 = MultipartEncoder(fields={
+        'csrfmiddlewaretoken':'4UctTYoxrU99MAqBHuVnUUG8ypptjUogbZykv49ASVBJBPMCULi0Vz5WIGjc6Wz0',
+        'node':'1', 
+        'cp_name':ystenlive, 
+        'access_mark':test, 
+        'live_srcs-TOTAL_FORMS':'1', 
+        'live_srcs-INITIAL_FORMS':'0', 
+        'live_srcs-MIN_NUM_FORMS':'1', 
+        'live_srcs-MAX_NUM_FORMS':'1000', 
+        'live_srcs-0-status':'1', 
+        'live_srcs-0-cp_name':ystenlive, 
+        'live_srcs-0-access_mark':test, 
+        'live_srcs-0-url': imitate[i%3], 
+        'live_srcs-0-parse_mode':'3', 
+        'live_srcs-0-priority':'9', 
+        'live_srcs-0-on_off':'on', 
+        'live_srcs-0-check_time':'1800', 
+        'live_srcs-0-retry_time':'30', 
+        'live_srcs-0-delay_time':'30', 
+        'live_srcs-0-warn_time':'30', 
+        'live_srcs-0-change_type':'0', 
+        'live_srcs-__prefix__-status':'1', 
+        'live_srcs-__prefix__-parse_mode':'0', 
+        'live_srcs-__prefix__-priority':'1', 
+        'live_srcs-__prefix__-on_off':'on', 
+        'live_srcs-__prefix__-check_time':'1800', 
+        'live_srcs-__prefix__-retry_time':'30', 
+        'live_srcs-__prefix__-delay_time':'30', 
+        'live_srcs-__prefix__-warn_time':'30', 
+        'live_srcs-__prefix__-change_type':'0',
+        })
+
+        m5 = MultipartEncoder(fields={
+        'csrfmiddlewaretoken':'OLT31o33HP1kzfUuARIAwjM1Ypsrw5ZuVQfUDuO68QtUougvN85dxYbP8Gmaj7ae',
+        'node':'1',
+        'cp':ystenlive,
+        'access_mark':test,
+        'url':imitate[i%3],
+        'tsDur':'10',
+        'ts_record_time':'168',
+        'ts_mode':'3',
+        'copies':'1',
+        'code_rate':'500000',
+        'channel':'cc'
+        })
+
+
+
+        r4 = requests.post(url4, data=m4, headers={'Content-Type': m4.content_type}, cookies=cookies)
+        print(r4.status_code)
+
+        params = {'id': i}
+
+        r4_get = requests.get(url4_get,cookies=cookies, params=params)
+        print(r4_get.status_code)
+
+
+    
+        r5 = requests.post(url5, data=m5, headers={'Content-Type': m5.content_type}, cookies=cookies)
+        print(r5.status_code)
+        r5_get = requests.get(url5_get,cookies=cookies, params=params)
+        print(r5_get.status_code)
+
+        time.sleep
+
+
+    time.sleep(2*60)
+
+    # delete 
+
+i = 174
+imitate = imitate_1k 
+ystenlive = 'ystenlive6'
+
+
+circle_add(i, imitate = imitate, ystenlive=ystenlive)
+
+
--- c++/study/test1.cpp
+++ c++/study/test1.cpp
@@ -1,22 +1,47 @@
-#include <string>
+#include<iostream>
+#include<string>
+#include<map>
 using namespace std;
-class Student{
-public:
-    static int total;
-    string name;
-    int age;
-    float scores;
-};
-int Student::total = 0;
-int  main(){
-    int n = 0;
-    const int &r = n;
-    Student stu;
-    decltype(n) a = n;  //n 为 int 类型，a 被推导为 int 类型
-    // 跟着前面的括号里面得类型走
-    decltype(r) b = n;     //r 为 const int& 类型, b 被推导为 const int& 类型
-    // 根据前面得变量类型 从而自动推导
-    decltype(Student::total) c = 0;  //total 为类 Student 的一个 int 类型的成员变量，c 被推导为 int 类型
-    decltype(stu.name) url = "http://c.biancheng.net/cplus/";  //total 为类 Student 的一个 string 类型的成员变量， url 被推导为 string 类型
+
+int main(int argc, char const *argv[])
+{
+    string name[]={"张三","李四","王麻子"};
+    double salary[]={1200,2000,1450};
+    map<string,double>sal;
+    map<string,double>::iterator p;
+    for(int i=0;i<3;i++){
+        sal.insert(make_pair(name[i],salary[i]));
+    }
+    sal["tom"]=6156;
+    sal["bob"]=5999;
+    for(p=sal.begin();p!=sal.end();p++){
+        cout<<p->first<<"\t"<<p->second<<endl;
+    }
+    string person;
+    cout<<"输入查找人员的姓名:";
+    cin>>person;
+    int flag=1;
+    for(p=sal.begin();p!=sal.end();p++)
+        if(p->first==person){
+            cout<<p->second<<endl;    
+            flag=0; 
+        }
+    if(flag)
+        cout<<"没查找到对应的结果！"<<endl; 
+
+    cout<<"输入待删除的人员的姓名:";
+    cin>>person;
+    map<string,double>::iterator it;
+    it = sal.find(person);
+    if(it!=sal.end()){
+        cout<<"查找成功："<<(*it).first<<":"<<(*it).second<<endl;
+        sal.erase(it);
+        cout<<"删除成功"<<endl;
+    }
+    cout<<"删除后的结果为"<<endl;
+    for(p=sal.begin();p!=sal.end();p++){
+        cout<<p->first<<p->second<<endl;
+    }
+
     return 0;
 }
