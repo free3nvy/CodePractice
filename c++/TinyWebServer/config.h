6fc9dbccbb57ec451a446a0c3be2f9bbc4d4dc56
Jacky Qu <free3nvy@gmail.com>
2023-02-27 12:28:32 +0800

    tiny

---
+++ c++/TinyWebServer/config.h
@@ -0,0 +1,10 @@
+# 如果没有定义过 则进行config_h的定义！！
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "webserver.h"
+
+using namespace std;
+
+
---
+++ c++/TinyWebServer/threadpool/threadpool.h
@@ -0,0 +1,16 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <list>
+#include <cstdio>
+#include <exception>
+#include <pthread.h>
+#include "../lock/locker.h"
+#include "../CGImysql/sql_connection_pool.h"
+
+template <typename T>
+class threadpool
+{
+public:
+    threadpool(int actor_model, connection_pool )
+}
