7a56546027bfd529635cc290a580d511a096d0c9
Jacky Qu <free3nvy@gmail.com>
2023-02-27 15:10:39 +0800

    tiny

---
+++ c++/TinyWebServer/lock/locker.h
@@ -0,0 +1,133 @@
+#ifndef LOCKER_H
+#define LOCKER_H
+
+
+#include <exception>
+#include <pthread.h>
+#include <semaphore>
+// 头文件里面实现函数内容！！
+
+class sem
+{
+public:
+    sem()
+    {
+        if (sem_init(&m_sem, 0, 0) != 0)
+        {
+            throw std::exception();
+        }
+    }
+    sem(int num)
+    {
+        if (sem_init(&m_sem, 0, num) != 0)
+        {
+            throw std::exception();
+        }
+    }
+
+    ~sem()
+    {
+        sem_destroy(&m_sem);
+    }
+
+    bool wait()
+    {
+        return sem_wait(&m_sem) == 0;
+    }
+
+    bool post()
+    {
+        return sem_post(&m_sem) == 0;
+    }
+
+private:
+    // 这是信号量的数据类型结构！！
+    sem_t m_sem;
+
+}
+
+
+class locker
+{
+public:
+    locker()
+    {
+        if (pthread_mutex_init(&m_mutex, NULL) != 0)
+        {
+            throw std::exception();
+        }
+    }
+
+    ~locker()
+    {
+        pthread_mutex_destroy(&m_mutex);
+    }
+    bool lock()
+    {
+        return pthread_mutex_lock(&m_mutex) == 0;
+    }
+    bool unlock()
+    {
+        return pthread_mutex_unlock(&m_mutex) == 0;
+    }
+
+    pthread_mutex_t *get()
+    {
+        return &m_mutex;
+    }
+
+private:
+    // m_ member 代码规范成员变量
+    pthread_mutex_t m_mutex;
+};
+
+
+// 条件类？？
+class cond
+{
+public:
+    cond()
+    {
+        if (pthread_cond_init(&m_cond, NULL) != 0)
+        {
+            throw std::exception();
+        }
+    }
+
+    ~cond()
+    {
+        pthread_cond_destroy(&m_cond);
+    }
+
+    bool wait(pthread_mutex_t *m_mutex)
+    {
+        int ret = 0;
+        ret = pthread_cond_wait(&m_cond, m_mutex);
+        return ret == 0;
+    }
+
+    bool timewait(pthread_mutex_t * m_mutex, struct timespec t)
+    {
+        int ret = 0;
+        ret = pthred_cond_timedwait(&m_cond, m_mutex, &t) ;
+
+        return ret == 0;
+    }
+
+    bool singal()
+    {
+        return pthread_cond_signal(&m_cond) == 0;
+    }
+    
+    bool broadcast()
+    {
+        return pthread_cond_broadcast(&m_cond) == 0;
+    }
+
+private:
+    pthread_cond_t m_cond;
+
+
+};
+
+#endif
