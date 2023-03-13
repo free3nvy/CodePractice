3263cb0def0488de14652c80d82c8bfe4ab2397c
Jacky Qu <free3nvy@gmail.com>
2023-03-13 20:55:10 +0800

    script

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,49 +1,17 @@
-#include <list>
 #include <iostream>
-#include <map>
+#include <stdio.h>
 using namespace std;
- 
- 
+
+class A{
+public:
+	int x;
+	virtual void b() {}
+};
+
 int main()
 {
-	
-	int id1, id2, num, n;
-	char str[10];
-	map< int, list<int> > ml;
-	cin >> n;
-	while (n--)
-	{
-		cin >> str;
-		switch (str[0])
-		{
-		case 'n':
-			cin >> id1;
-			ml[id1] = list<int>();
-			break;
-		case 'a':
-			cin >> id1 >> num;
-			ml[id1].push_back(num);
-			break;
-		case 'm':
-			cin >> id1 >> id2;
-			ml[id1].merge(ml[id2]);
-			break;
-		case 'u':
-			cin >> id1;
-			ml[id1].sort();
-			ml[id1].unique();
-			break;
-		case 'o':
-			cin >> id1;
-			ml[id1].sort();
-			for (auto iter = ml[id1].begin(); iter != ml[id1].end(); ++iter)
-			{
-				cout << *iter << " ";
-			}
-			cout << endl;
-			break;
-		}
-	}
- 
+	A* p = new A;
+	cout << p << endl;
+	cout << &p->x << endl;
 	return 0;
 }
