d38e6d808957a52e39488ba65504e93e63bd015b
Jacky Qu <free3nvy@gmail.com>
2023-03-28 17:19:11 +0800

    script

---
+++ leetcode_c++/232.用栈实现队列.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=232 lang=cpp
+ *
+ * [232] 用栈实现队列
+ */
+
+// @lc code=start
+class MyQueue {
+public:
+
+    Stack<int> stIn;
+    stack<int> stOut;
+
+    MyQueue() {
+        
+
+    }
+    
+    void push(int x) {
+
+    }
+    
+    int pop() {
+
+    }
+    
+    int peek() {
+
+    }
+    
+    bool empty() {
+
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
+
